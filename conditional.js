78c3e474078a3f07ada2707e3f299da68ba2b8aa
Shubhangisaini205 <shubhangisaini205@gmail.com>
2022-10-14 21:40:11 +0530

    conditional statement IW

---
+++ conditional.js
@@ -0,0 +1,4 @@
+https://replit.com/@ShubhangiSaini1/conditionaljs#P1.js
+https://replit.com/@ShubhangiSaini1/conditionaljs#P2.js
+https://replit.com/@ShubhangiSaini1/conditionaljs#P3.js
+https://replit.com/@ShubhangiSaini1/conditionaljs#P4.js
