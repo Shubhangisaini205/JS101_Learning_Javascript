8d2f7444d817ad452a3a4ccaf4ebdcd14a81ad52
Shubhangisaini205 <shubhangisaini205@gmail.com>
2022-10-14 21:27:04 +0530

    Day 1st IW

---
+++ variable.js
@@ -0,0 +1,4 @@
+https://replit.com/@ShubhangiSaini1/variablejs#problem1.js
+https://replit.com/@ShubhangiSaini1/variablejs#problem2.js
+https://replit.com/@ShubhangiSaini1/variablejs#problem3.js
+https://replit.com/@ShubhangiSaini1/variablejs#problem4.js
