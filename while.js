f8a10cfdafadc574d06ffa9a1f817b0a9534f70e
Shubhangisaini205 <shubhangisaini205@gmail.com>
2022-10-14 21:54:28 +0530

    while loop IW

---
+++ while.js
@@ -0,0 +1,4 @@
+https://replit.com/@ShubhangiSaini1/while-loopjs#Problem1.js
+https://replit.com/@ShubhangiSaini1/while-loopjs#Problem2.js
+https://replit.com/@ShubhangiSaini1/while-loopjs#Problem3.js
+https://replit.com/@ShubhangiSaini1/while-loopjs#Problem4.js
