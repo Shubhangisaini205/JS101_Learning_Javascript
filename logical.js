84fcf6cc2ebd1008b3d5be8db3a76d529ee7e8f6
Shubhangisaini205 <shubhangisaini205@gmail.com>
2022-10-14 21:47:41 +0530

    Logical IW

---
+++ logical.js
@@ -0,0 +1,5 @@
+https://replit.com/@ShubhangiSaini1/logicaljs#Problem1.js
+https://replit.com/@ShubhangiSaini1/logicaljs#Problem2.js
+https://replit.com/@ShubhangiSaini1/logicaljs#Problem3.js
+https://replit.com/@ShubhangiSaini1/logicaljs#Problem4.js
+https://replit.com/@ShubhangiSaini1/logicaljs#Problem5.js
